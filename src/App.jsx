f674becc5b5fb880ec19f8c8a602497feef51695
Liamm95 <liammeeka@gmail.com>
2026-04-04 22:41:45 +0900

    feat: add list of notes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,28 @@ const App = () => {
   return (
     <div className="notebook">
       <h1 className="notebook__title">NOTEBOOK AI</h1>
+      {/* Список заметок */}
+      <div className="notebook__notes">
+        <div className="note">
+          <p className="note__text">Привет! Я AI блокнот</p>
+          <button className="note__delete">✖</button>
+        </div>
+      </div>
+      {/* Форма для добавления новой заметки */}
       <form className="notebook__note">
         <div className="notebook__field field">
-          <label htmlFor="new-note" className="field__label"></label>
+          <label htmlFor="new-note" className="field__label">
+            Write your note here...
+          </label>
+          <input
+            type="text"
+            className="field__input"
+            id="new-note"
+            placeholder=" "
+            autoComplete="off"
+          />
         </div>
+        <button className="button" type="submit"></button>
       </form>
     </div>
   );
