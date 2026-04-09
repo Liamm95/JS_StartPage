904ca666100b73d9acde76721bab5be5249c99c0
Liamm95 <liammeeka@gmail.com>
2026-04-10 01:30:24 +0900

    name

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-const projectName = "🔥 ТЕСТ 123 🔥";
+const projectName = "NOTEBOOK WITH AI";
 
 const App = () => {
   return (
@@ -7,7 +7,7 @@ const App = () => {
       {/* Список заметок */}
       <div className="app__layout">
         <div className="notebook">
-          <h2 className="notebook__title">Зам</h2>
+          <h2 className="notebook__title">Заметки</h2>
           <div className="notebook__tabs">
             <button className="tab tab--active">Заметка 1</button>
             <button className="tab">Заметка 2</button>
